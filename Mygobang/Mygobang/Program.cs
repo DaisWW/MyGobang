b1639da9e05431f3c5d0b3f4f8a8418e0572017f
Dais <651010017@qq.com>
2018-12-23 14:48:53 +0800

    Update Program.cs
    test

--- Mygobang/Mygobang/Program.cs
+++ Mygobang/Mygobang/Program.cs
@@ -8,6 +8,16 @@ class Program
     static void Main(string[] args)
     {
 
+        string read = Console.ReadLine();
 
+        Console.WriteLine("刚刚输入的是:"+ read);
+
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("请按任意键退出...");
+        Console.ReadKey();
+        
     }
+    
 }
